a06f9e759bf670dfb7d7cb0ae8ae86b1d83a9987
aletheia <bianchi.luca@gmail.com>
2020-12-07 14:52:29 +0100

    :sparkles: Add lambda inference. refs #3
    Add code for lambda function doing inference

--- lambda/inference/index.py
+++ lambda/inference/index.py
@@ -1,12 +1,19 @@
 
 import json
 
+from classifier import ChurnClassifier
+
 def handler(event, context):    
+    #body = json.loads(event['body'])
     body = json.loads(event['body'])
     print(body)
+    classifier = ChurnClassifier()
+    res = classifier.predict(body)
+    print('Got data')
+    print(res)
     return {
         'statusCode': 200,
-        'body': json.dumps(body),
+        'body': json.dumps({"churn status":res}),
         'headers': {
             'Access-Control-Allow-Origin': True,
             'Access-Control-Allow-Origin': "' *'",
